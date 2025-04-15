3ffd38fda2959d3ea0c474742a70a7b5ef75d150
Thomas DRAGHI <thomasdraghi@gmail.com>
2025-04-15 10:39:47 +0200

    Afichage_articles

---
+++ src/main/java/com/eni/encheres/dao/IDAOArticleVendu.java
@@ -0,0 +1,11 @@
+package com.eni.encheres.dao;
+
+import com.eni.encheres.bo.ArticleVendu;
+
+import java.util.List;
+
+public interface IDAOArticleVendu {
+
+        List<ArticleVendu> selectAll();
+
+}
---
+++ src/main/java/com/eni/encheres/dao/IDAOEnchere.java
@@ -0,0 +1,10 @@
+//package com.eni.encheres.dao;
+//
+//import com.eni.encheres.bo.Enchere;
+//
+//import java.util.List;
+//
+//public interface IDAOEnchere {
+//
+//    List<Enchere> selectAll();
+//}

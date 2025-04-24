43782e32bd3ebb92c1e3f8883985e3cc2a2965d2
Lucas-eni <lucas.robichon2025@campus-eni.fr>
2025-04-24 12:29:29 +0200

    ajout role disabled et admin

--- src/main/java/com/eni/encheres/security/UtilisateurSpringSecurity.java
+++ src/main/java/com/eni/encheres/security/UtilisateurSpringSecurity.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -25,12 +26,17 @@ public class UtilisateurSpringSecurity implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         // si le membre est admin
+        List<GrantedAuthority> authorities = new ArrayList<>();
         if (utilisateur.isAdmin()) {
             // je lui donne le rôle "admin" (équivalent à la permission "ROLE_admin")
-            return List.of(new SimpleGrantedAuthority("ROLE_admin"));
+            authorities.add(new SimpleGrantedAuthority("ROLE_admin"));
+        }
+        if(utilisateur.isDisabled()) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_disabled"));
         }
         // sinon je lui donne le rôle "user (équivalent à la permission "ROLE_user")
-        return List.of(new SimpleGrantedAuthority("ROLE_user"));
+        authorities.add(new SimpleGrantedAuthority("ROLE_user"));
+        return authorities;
     }
 
     /**

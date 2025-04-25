00a6cd1cf5eb6cb4985178e39625cf3dd99cef1f
Lucas-eni <lucas.robichon2025@campus-eni.fr>
2025-04-25 11:22:54 +0200

    Bouton pour changer la langue
    Bouton pour changer la langue

--- src/main/java/com/eni/encheres/config/WebConfig.java
+++ src/main/java/com/eni/encheres/config/WebConfig.java
@@ -1,10 +1,16 @@
 package com.eni.encheres.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -17,4 +23,22 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/articles/");
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.FRENCH); // Langue par défaut = 🇫🇷
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang"); // Utilise ?lang=en dans l'URL
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
